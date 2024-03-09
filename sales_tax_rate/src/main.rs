a4c6d77b0f19c336285f189abd1e3dd5eec6adee
gustavo <gustsanc@gmail.com>
2024-03-09 21:03:14 +0100

    [2.Use Dapr to discover & invoke uservices]
    2 - In the sales_tax_rate service, we will read zip code and tax rate data from the CSV file and import it into a key-value store through the Dapr sidecar (i.e., the client). As you can see, we need to structure the data into key/value JSON objects before we can save them.
    
    3 - Then, in the find_rate handler, look up the zip code from the key-value store through the Dapr sidecar.
    
    Deliverable
    
    The deliverable for this milestone is a complete application refactored to use the Dapr state store to manage the zip code and tax rate data table.
    
    Upload a link to your deliverable in the Submit Your Work section and click submit. After submitting, the author’s solution and peer solutions will appear on the page for you to examine.

--- sales_tax_rate/src/main.rs
+++ sales_tax_rate/src/main.rs
@@ -1,9 +1,12 @@
 use std::net::SocketAddr;
 use std::convert::Infallible;
+use std::time::Duration;
 use hyper::service::{make_service_fn, service_fn};
 use hyper::{Body, Method, Request, Response, StatusCode, Server};
-use serde_json::Value;
+use serde_json::{json, Value};
 use csv::Reader;
+use serde_json::Value::String;
+use tokio::time::sleep;
 
 /// This is our service handler. It receives a Request, routes on its
 /// path, and returns a Future of a Response.
@@ -18,25 +21,15 @@ async fn handle_request(req: Request<Body>) -> Result<Response<Body>, anyhow::Er
             let byte_stream = hyper::body::to_bytes(req).await?;
             let json : Value = serde_json::from_slice(&byte_stream).unwrap();
             let zip = json["zip"].as_str().unwrap();
-            let mut rate = "".to_string();
 
-            let rates_data: &[u8] = include_bytes!("rates_by_zipcode.csv");
-            let mut rdr = Reader::from_reader(rates_data);
-            for result in rdr.records() {
-                let record = result?;
-                // dbg!("{:?}", record.clone());
-                if zip.eq(&record[0]) {
-                    rate = record[1].to_string();
-                    break;
+            let client = dapr::Dapr::new(3501);
+            match client.get_state("statestore", zip).await? {
+                String(rate) => {
+                    Ok(Response::new(Body::from(rate)))
+                },
+                _ => {
+                    Ok(Response::new(Body::from("Not Found")))
                 }
-            }
-
-            if rate.is_empty() {
-                let mut not_found = Response::default();
-                *not_found.status_mut() = StatusCode::NOT_FOUND;
-                Ok(not_found)
-            } else {
-                Ok(Response::new(Body::from(rate)))
             }
         }
 
@@ -51,6 +44,20 @@ async fn handle_request(req: Request<Body>) -> Result<Response<Body>, anyhow::Er
 
 #[tokio::main(flavor = "current_thread")]
 async fn main() -> Result<(), Box<dyn std::error::Error + Send + Sync>> {
+    println!("App started. Wait for Dapr sidecar to start ...");
+    sleep(Duration::from_millis(1500)).await;
+
+    let client = dapr::Dapr::new(3501);
+    let rates_data: &[u8] = include_bytes!("rates_by_zipcode.csv");
+    let mut rdr = Reader::from_reader(rates_data);
+    for result in rdr.records() {
+        let record = result?;
+        let kvs = json!([{
+        "key": record[0], "value": record[1]
+    }]);
+        client.save_state("statestore", kvs).await?;
+    }
+
     let addr = SocketAddr::from(([0, 0, 0, 0], 8001));
     let make_svc = make_service_fn(|_| {
         async move {
